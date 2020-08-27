01a02ce32c89de83248693e9307cc9f88fbc8516
Anonymous <anonymous@ubuntu.ubuntu-domain>
2020-08-27 21:02:00 +0700

    Commit API & Data

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
     if(pathName === '/' || pathName === '/overview ') {
         res.end('This is the overview');
     } else if(pathName === '/product') {
         res.end('This is the product');
+    } else if(pathName === '/api') {
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(data);
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
